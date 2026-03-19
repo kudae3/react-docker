c2ff29328890cd7b25abdb9c5b39aa5a46991b3c
Kudae Sithu <kudaesithu2@gmail.com>
2026-03-19 13:39:33 +0630

    add Dockerfile, .dockerignore, and docker-compose configuration; update README and App component title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Docker is Cool!</h1>
+          <h1>Docker is Fun!</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
